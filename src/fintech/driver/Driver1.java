9d6b2297ba41287a7eb92fbeaa2ae9afc3631774
alifsuedi <143678957+alifsuedi@users.noreply.github.com>
2025-02-07 17:44:04 +0700

    pair 23

--- src/fintech/driver/Driver1.java
+++ src/fintech/driver/Driver1.java
@@ -8,22 +8,18 @@ import fintech.model.Account;
  * @author NIM Nama
  * @author NIM Nama
  */
-public class Driver1 {
+
+ public class Driver1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Read account creation input
         String command = scanner.nextLine();
         String owner = scanner.nextLine();
         String accountName = scanner.nextLine();
 
-        if ("create-account".equals(command)) {
-            Account account = new Account(command, owner, accountName);
-            System.out.println(account.toString());
-        } else {
-            System.out.println("Invalid command");
-        }
+        Double balance = 0.0;
 
+        System.out.println(owner + "|" + accountName + "|" + balance);
         scanner.close();
     }
-}
+ }
